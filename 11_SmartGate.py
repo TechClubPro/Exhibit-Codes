6848f0b2ef5b67316af123ee35152c4973cb27da
TechClubPro <62166836+TechClubPro@users.noreply.github.com>
2021-11-12 14:13:53 +0530

    Update 11_SmartGate.py

--- 11_SmartGate.py
+++ 11_SmartGate.py
@@ -1,63 +1,46 @@
-# -*- coding: utf-8 -*-
 """
-Created on Fri Feb 28 12:54:52 2020
-
-@author: Toshiba
+Program to Read the State of Object Sensor 
+and Display it on Console.
 """
 
-import Phygital as PyBot
-
+from Phygital_v0 import Phygital_v0 as pyro
 import time
-import pyttsx3
-
-engine = pyttsx3.init() # object creation
-
-""" RATE"""
-rate = engine.getProperty('rate')   # getting details of current speaking rate
-print (rate)                        #printing current voice rate
-engine.setProperty('rate', 125)     # setting up new voice rate
 
 
-"""VOLUME"""
-volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
-print (volume)                          #printing current volume level
-engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
-
-"""VOICE"""
-voices = engine.getProperty('voices')       #getting details of current voice
-#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
-engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
-PyBot.init("COM5")
-PyBot.MoveServo(3,10)
+# Pin Initialization
+pyro.pinMode('A0','dInput')
+pyro.pinMode('A2','dInput')
+pyro.pinMode('A5','dInput')
+#Communication Init
+pyro.init("COM7")
 
 while True:
     try:
-        entrySensor = PyBot.dRead(1)
-        exitSensor  = PyBot.dRead(2)
-        
-        
-        if entrySensor==1 and exitSensor==0:
            
-            engine.say("Someone's at the entry,Opening the door")
-            engine.runAndWait() 
-            PyBot.MoveServo(3,90)
-            
-            
-        if entrySensor==0 and exitSensor==1:
-           
-            engine.say("Exit point, closing the door")
-            engine.runAndWait() 
-            PyBot.MoveServo(3,10)
-            
-            
+        #Read State of Sensor
+        data1=pyro.dRead('A0')
+        data2=pyro.dRead('A2')
+#        data3=pyro.dRead('A5')
+        print("Sensor State: "+ str(data1) + str(data2))
+        time.sleep(1)
+     
+      
+        if data1==0:# Object Sensed
+            print("Object Sensed")
+	       
+            pyro.MoveServo(9,10)
+            time.sleep(1)            
+        else:
+            print("No Object")
+	        
+            pyro.MoveServo(9,90) 
+            time.sleep(1)
+      
         
-            
+    
+    
     except:
         if KeyboardInterrupt:
-            PyBot.close()
+            pyro.close()
             break
 print("Closing")
-                
-        
-        
-        
